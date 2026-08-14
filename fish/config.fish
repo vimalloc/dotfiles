23d62d60d4204e92d717eeedb33b786c4cbb1c20
Lily Acadia Gilbert <lily@informedk12.com>
2026-08-14 14:23:01 -0600

    Fix fish todo stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,3 +1,6 @@
 if status is-interactive
-    # Commands to run in interactive sessions can go here
+  # Commands to run in interactive sessions can go here
+
+  set --global fish_key_bindings fish_default_key_bindings
+  fish_config theme choose "catppuccin-frappe"
 end
