ea777129178df82073e69a5f704be7ddee86eb2e
Lily Acadia Gilbert <lily@informedk12.com>
2026-08-14 22:45:42 -0600

    Install dial.nvim

---
+++ nvim/lua/plugins/dial.lua
@@ -0,0 +1,38 @@
+-- Ideas
+--   to_i => Integer
+--   find => where
+
+return {
+  "monaqa/dial.nvim",
+  keys = {
+    { "<C-a>",
+      function() require("dial.map").manipulate("increment", "normal") end,
+      desc = "Dial Increment"
+    },
+    { "<C-x>",
+      function() require("dial.map").manipulate("decrement", "normal") end,
+      desc = "Dial Decrement"
+    },
+  },
+  config = function()
+    local dial_config = require("dial.config")
+    local augend = require("dial.augend")
+
+    dial_config.augends:register_group({
+      default = {
+        augend.integer.alias.decimal,
+        augend.integer.alias.hex,
+        augend.constant.new({
+          elements = { "true", "false" },
+          word = true,
+          cyclic = true,
+        }),
+        augend.constant.new({
+          elements = { "success", "failure" },
+          word = true,
+          cyclic = true,
+        }),
+      },
+    })
+  end,
+}
