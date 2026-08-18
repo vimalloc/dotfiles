6fb33cf55101e4b7e2093a607b632c37c215c695
Lily Acadia Gilbert <lily@informedk12.com>
2026-08-18 13:09:45 -0600

    Ruby dap
    Requires debug in Gemfile, gotta convince team of this

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -1,6 +1,11 @@
 return {
   'mfussenegger/nvim-dap',
+  dependencies = {
+    "suketa/nvim-dap-ruby",
+  },
   config = function ()
+    require("dap-ruby").setup()
+
     local dap = require("dap")
 
     -- Execution & Sessions
