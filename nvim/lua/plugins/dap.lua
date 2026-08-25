7e2da1ffc7e1a215a9cdaf6072eb9eaece64ee47
Lily Acadia Gilbert <lily@informedk12.com>
2026-08-25 10:19:19 -0600

    Change keybing for neotest and nvim-dap.

--- nvim/lua/plugins/dap.lua
+++ nvim/lua/plugins/dap.lua
@@ -52,18 +52,24 @@ return {
 
     -- Keymaps: Maybe find better keys, keep doing 'bd' instead and closing window
     -- I know some configs suggested the F keys, maybe those??
-    vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'Debug: Continue / Start' })
-    vim.keymap.set('n', '<leader>dx', dap.terminate, { desc = 'Debug: Terminate Session' })
-    vim.keymap.set('n', '<leader>dr', dap.repl.open, { desc = 'Debug: Open REPL' })
-    vim.keymap.set('n', '<leader>dl', dap.run_last, { desc = 'Debug: Run Last Session' })
-    vim.keymap.set('n', '<leader>dg', dap.step_over, { desc = 'Debug: Step Over' })
-    vim.keymap.set('n', '<leader>di', dap.step_into, { desc = 'Debug: Step Into' })
-    vim.keymap.set('n', '<leader>do', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<leader>db', dap.run_to_cursor, { desc = 'Debug: Run to Cursor' })
+    -- TODO - move these into an autocommand to not have them live in normal code??
+    -- TODO - dc == continue only, not start. That's neotests job
+    vim.keymap.set('n', '<Right>', dap.step_into, { desc = 'Debug: Step Over' })
+    vim.keymap.set('n', '<Left>', dap.step_out, { desc = 'Debug: Step Out' })
+    vim.keymap.set('n', '<Down>', dap.step_over, { desc = 'Debug: Step Into' })
+    vim.keymap.set('n', '<Up>', dap.restart_frame, { desc = 'Debug: Step Into' })
+
     vim.keymap.set('n', '<leader>dt', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
+    vim.keymap.set('n', '<leader>db', dap.run_to_cursor, { desc = 'Debug: Run to Cursor' })
+    vim.keymap.set('n', '<leader>dc', dap.continue, { desc = 'Debug: Continue / Start' })
     vim.keymap.set('n', '<leader>dC', function()
       dap.set_breakpoint(vim.fn.input('Breakpoint condition: '))
     end, { desc = 'Debug: Conditional Breakpoint' })
+
+    vim.keymap.set({'n', 'v'}, '<leader>dr', function()
+      require("dap-view").open()
+    end, { desc = 'Open Repl' })
+
     vim.keymap.set({'n', 'v'}, '<leader>dh', function()
       require('dap.ui.widgets').hover()
     end, { desc = 'Debug: Hover Variables' })
--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -12,5 +12,22 @@ return {
         require("neotest-rspec")
       },
     })
+
+    -- TODO debug file isn't working
+    vim.keymap.set('n', '<leader>tn', function()
+      require("neotest").run.run()
+    end, { desc = 'Run nearest test' })
+
+    vim.keymap.set('n', '<leader>dn', function()
+      require("neotest").run.run({strategy = "dap"})
+    end, { desc = 'Debug nearest test' })
+
+    vim.keymap.set('n', '<leader>tf', function()
+      require("neotest").run.run(vim.fn.expand("%"))
+    end, { desc = 'Run test file' })
+
+    vim.keymap.set('n', '<leader>df', function()
+      require("neotest").run.run(vim.fn.expand("%"), {strategy = "dap"})
+    end, { desc = 'Debug file' })
   end
 }
