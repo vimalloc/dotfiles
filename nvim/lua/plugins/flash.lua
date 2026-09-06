b72d13f66b30abea9f24ba6123db33870fc50fe6
Lily Acadia Gilbert <lily@informedk12.com>
2026-09-06 11:57:55 -0600

    Cleanup flash config

--- nvim/lua/plugins/flash.lua
+++ nvim/lua/plugins/flash.lua
@@ -1,34 +1,18 @@
 return {
   "folke/flash.nvim",
   event = "VeryLazy",
-  opts = {
-    modes = {
-      char = {
-        keys = {}, -- Empty this to unbind builtin f/F/t/T handling (i hate this)
-      },
-    },
-  },
   keys = {
+    { "t", false },
+    { "T", false },
     { "f",
       mode = { "n", "x", "o" },
       function() require("flash").jump() end,
-      desc = "Flash"
+      desc = "Flash Jump"
     },
     { "F",
       mode = { "n", "x", "o" },
-      function() require("flash").treesitter() end,
-      desc = "Flash Treesitter"
-    },
-    { "r",
-      mode = "o",
-      function() require("flash").remote() end,
-      desc = "Remote Flash"
-    },
-    {
-      "R",
-      mode = { "o", "x" },
       function() require("flash").treesitter_search() end,
-      desc = "Treesitter Search"
+      desc = "Flash Treesitter Search"
     },
   },
 }
