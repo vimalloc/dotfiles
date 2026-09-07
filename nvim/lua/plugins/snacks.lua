85f3abbc898844535c9f89a5659c63ab67d41dd9
Lily Acadia Gilbert <lily@informedk12.com>
2026-09-06 19:30:47 -0600

    Make notify stay for smaller.
    Things like code actions last too long and can get in the way of things.
    Remember we can use leader n to get the message if we need it longer.

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -15,7 +15,7 @@ return {
     input = { enabled = true },
     notifier = {
       enabled = true,
-      timeout = 10000,
+      timeout = 5000,
       width = { min = 40, max = 0.6 },
       height = { min = 1, max = 0.8 },
     },
