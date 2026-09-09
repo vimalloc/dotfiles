42ce6beeeae132ee92dcb07cc5b2cc8b2cc924b9
Lily Acadia Gilbert <lily@informedk12.com>
2026-09-09 11:18:21 -0600

    Make [[ and ]] work more with the direction I want

--- nvim/after/ftplugin/ruby.lua
+++ nvim/after/ftplugin/ruby.lua
@@ -1,5 +1,5 @@
 local forward = function() Snacks.words.jump(1) end
 local backward = function() Snacks.words.jump(-1) end
 
-vim.keymap.set({ "n", "t" }, "[[", forward, { buffer = true, silent = true })
-vim.keymap.set({ "n", "t" }, "]]", backward, { buffer = true, silent = true })
+vim.keymap.set({ "n", "t" }, "[[", backward, { buffer = true, silent = true })
+vim.keymap.set({ "n", "t" }, "]]", forward, { buffer = true, silent = true })
