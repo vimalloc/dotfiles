335cafd48ec591f16d2f914e0188fac2773c57a1
Lily Acadia Gilbert <lily@informedk12.com>
2026-09-17 10:42:27 -0600

    Updaes and homebrew autocompletion

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,15 @@ if status is-interactive
     history save
     history merge
   end
+
+  # Heroku autocompletion
+  if test -d (brew --prefix)"/share/fish/completions"
+    set -p fish_complete_path (brew --prefix)/share/fish/completions
+  end
+
+  if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+    set -p fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+  end
 end
 
 # Add docker to path
