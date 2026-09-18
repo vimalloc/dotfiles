9808a9dbc804cd95159bf5d973ffb64f38ea2d5b
Lily Acadia Gilbert <lily@informedk12.com>
2026-09-17 21:20:19 -0600

    Simplify and improve skip_concealed_text

--- nvim/after/plugin/skip_concealed_text.lua
+++ nvim/after/plugin/skip_concealed_text.lua
@@ -1,38 +1,30 @@
--- TODO:
---   * External links have conceal in multiple locations in the markdown, some of which
---     which are not in extmarks at all
---   * If last items in column are concealed, do not move cursor over them
---   * Support going forward or backward.
-local function is_concealed(row, column)
-  local marks = vim.api.nvim_buf_get_extmarks(
-    0, -1, { row, column }, { row, column },
-    { details = true, overlap = true }
-  )
-
-  -- what does mcolumn mean? Better naming?
-  for _, mark in ipairs(marks) do
-    local mcolumn, details = mark[3], mark[4]
-    if details.conceal ~= nil then
-      local end_column = details.end_column or (mcolumn + 1)
-      if column >= mcolumn and column < end_column then
-        return true
-      end
-    end
-  end
-  return false
-end
-
 vim.api.nvim_create_user_command("SkipConcealedText", function()
   local window_id = vim.api.nvim_get_current_win()
   local row, column = unpack(vim.api.nvim_win_get_cursor(window_id))
-  local line = vim.api.nvim_buf_get_lines(0, row - 1, row, false)[1]
+  local marks = vim.api.nvim_buf_get_extmarks(
+    0,
+    -1,
+    { row - 1, column },
+    { row - 1, column + 1 },
+    { details = true, overlap = true }
+  )
 
-  while column < #line do
-    column = column + 1
-    if not is_concealed(row, column) then
-      break
+  for _, mark in ipairs(marks) do
+    local details = mark[4]
+    if details.conceal ~= nil then
+      vim.api.nvim_win_set_cursor(window_id, { row, details.end_col + 1 })
+      return
     end
   end
-
-  vim.api.nvim_win_set_cursor(window_id, { row, column })
+  vim.api.nvim_win_set_cursor(window_id, { row, column + 1 })
 end, { desc = "Skip Concealed Text" })
+
+
+-- TODO:
+--   * External links should skip to the end of the link, not to the start of it.
+--     Something with end_col probably. Figure out what to do there
+--     * Fix: If there are multiple marks that have conceal, find the one that has
+--       the larger value. Should fix this.
+--   * Have this work for both right and left
+--   * Nvim 13 has a new feature for next wrapping on external links I believe.
+--     Figure that out when it drops
