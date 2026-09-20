9574b23b7d60d882d694cd06f164ccd995f65d7b
Lily Acadia Gilbert <lily@informedk12.com>
2026-09-20 11:28:32 -0600

    Fix navigating to end of external link

--- nvim/after/plugin/skip_concealed_text.lua
+++ nvim/after/plugin/skip_concealed_text.lua
@@ -1,30 +1,29 @@
 vim.api.nvim_create_user_command("SkipConcealedText", function()
   local window_id = vim.api.nvim_get_current_win()
-  local row, column = unpack(vim.api.nvim_win_get_cursor(window_id))
+  local row, col = unpack(vim.api.nvim_win_get_cursor(window_id))
   local marks = vim.api.nvim_buf_get_extmarks(
     0,
     -1,
-    { row - 1, column },
-    { row - 1, column + 1 },
+    { row - 1, col },
+    { row - 1, col + 1 },
     { details = true, overlap = true }
   )
 
+  next_col = col + 1
   for _, mark in ipairs(marks) do
     local details = mark[4]
     if details.conceal ~= nil then
-      vim.api.nvim_win_set_cursor(window_id, { row, details.end_col + 1 })
-      return
+      possible_next_col = details.end_col + 1
+      if possible_next_col > next_col then
+        next_col = possible_next_col
+      end
     end
   end
-  vim.api.nvim_win_set_cursor(window_id, { row, column + 1 })
+  vim.api.nvim_win_set_cursor(window_id, { row, next_col })
 end, { desc = "Skip Concealed Text" })
 
 
 -- TODO:
---   * External links should skip to the end of the link, not to the start of it.
---     Something with end_col probably. Figure out what to do there
---     * Fix: If there are multiple marks that have conceal, find the one that has
---       the larger value. Should fix this.
 --   * Have this work for both right and left
 --   * Nvim 13 has a new feature for next wrapping on external links I believe.
 --     Figure that out when it drops
