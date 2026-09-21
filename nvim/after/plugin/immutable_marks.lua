fcae9e845f36323a9c558ab206a7296b9a444355
Lily Acadia Gilbert <lily@informedk12.com>
2026-09-20 19:15:31 -0600

    Allow you to reset mark if pressed in rapid succesesion

--- nvim/after/plugin/immutable_marks.lua
+++ nvim/after/plugin/immutable_marks.lua
@@ -1,3 +1,6 @@
+local last_ran_time = 0
+local delta_to_allow_reset_mark = 500
+
 vim.api.nvim_create_user_command("ImmutableMarks", function()
   -- Bail if escape. Probably more codes should be here, but only one I can think of
   local new_mark = vim.fn.getcharstr()
@@ -7,14 +10,16 @@ vim.api.nvim_create_user_command("ImmutableMarks", function()
 
   local buffer = vim.api.nvim_get_current_buf()
   local all_marks = vim.fn.getmarklist(buffer)
-  local map_keys = function(mark_object)
-    return mark_object.mark
-  end
+  local map_keys = function(mark_object) return mark_object.mark end
   local marks = vim.tbl_map(map_keys, all_marks)
+  local new_formatted_mark = "\'" .. new_mark
+  local mark_exists = vim.list_contains(marks, new_formatted_mark)
 
-  new_formatted_mark = "\'" .. new_mark
-  mark_exists = vim.list_contains(marks, new_formatted_mark)
-  if mark_exists then
+  local current_time = vim.uv.hrtime() / 1e6
+  local elapsed = current_time - last_ran_time
+  last_ran_time = current_time
+
+  if mark_exists and elapsed > delta_to_allow_reset_mark then
     vim.notify("Mark already exists. Use :delmarks! to remove", vim.log.levels.ERROR)
     return
   end
