9c229d829c0fd61390e28a96f74a46cb5e0cf4c1
Lily Acadia Gilbert <lily@informedk12.com>
2026-09-20 20:08:03 -0600

    clean up immutable marks

--- nvim/after/plugin/immutable_marks.lua
+++ nvim/after/plugin/immutable_marks.lua
@@ -1,31 +1,37 @@
 local last_ran_time = 0
 local delta_to_allow_reset_mark = 500
 
+local function last_ran_within_delta()
+  local current_time = vim.uv.hrtime() / 1000000
+  local elapsed = current_time - last_ran_time
+
+  last_ran_time = current_time
+
+  return elapsed > delta_to_allow_reset_mark
+end
+
 vim.api.nvim_create_user_command("ImmutableMarks", function()
-  -- Bail if escape. Probably more codes should be here, but only one I can think of
-  local new_mark = vim.fn.getcharstr()
-  if string.byte(new_mark) == 27 then
+  -- Bail if escape key
+  local mark = vim.fn.getcharstr()
+  if string.byte(mark) == 27 then
     return
   end
 
   local buffer = vim.api.nvim_get_current_buf()
-  local all_marks = vim.fn.getmarklist(buffer)
-  local map_keys = function(mark_object) return mark_object.mark end
-  local marks = vim.tbl_map(map_keys, all_marks)
-  local new_formatted_mark = "\'" .. new_mark
-  local mark_exists = vim.list_contains(marks, new_formatted_mark)
+  local mark_objects = vim.fn.getmarklist(buffer)
+  local mark_object_to_char = function(mark_object)
+    return mark_object.mark:gsub("\'", "")
+  end
+  local marks = vim.tbl_map(mark_object_to_char, mark_objects)
+  local mark_exists = vim.list_contains(marks, mark)
 
-  local current_time = vim.uv.hrtime() / 1e6
-  local elapsed = current_time - last_ran_time
-  last_ran_time = current_time
-
-  if mark_exists and elapsed > delta_to_allow_reset_mark then
-    vim.notify("Mark already exists. Use :delmarks! to remove", vim.log.levels.ERROR)
+  if mark_exists and last_ran_within_delta() then
+    vim.notify("Mark already exists", vim.log.levels.ERROR)
     return
   end
 
   local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-  vim.api.nvim_buf_set_mark(buffer, new_mark, row, col, {})
+  vim.api.nvim_buf_set_mark(buffer, mark, row, col, {})
 end, { desc = "Immutable Marks" })
 
 vim.keymap.set('n', 'm', '<cmd>ImmutableMarks<cr>')
